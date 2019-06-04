64a81c0a23de6f3d838a26affd44810eaef417ec
Adriano Guedes <guedessoftware@gmail.com>
2019-06-04 06:18:17 +0000

    Esbarrei em um erro que nao consigo solucionar, ja revisei o codigo e nao consigo...

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php"); //carrega todas as dependencias do site
 
 use \Slim\Slim; //chama a classe Slim 
 use \Hcode\Page; //chama da classe Page
 use \Hcode\PageAdmin; //chama da classe PageAdmin
+use \Hcode\Model\User;
 
 $app = new Slim(); //instanciando a classe slim
 
@@ -19,9 +22,41 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index"); //chamando o conteudo da pagina
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+	
+	header("location: /admin");
+
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+
+	exit;
+
 });
 
 $app->run(); //roda todo o codigo depois de ter carregado tudo
