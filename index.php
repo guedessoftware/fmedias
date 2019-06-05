69290bd1f9ad7cc2203243b0a9c3f6ae6b56d58d
Adriano Guedes <guedessoftware@gmail.com>
2019-06-05 17:07:23 +0000

    construindo cadastro de usuarios

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $app->get('/admin/login', function(){
 $app->post('/admin/login', function() {
 
 	User::login($_POST["login"], $_POST["password"]);
-	
+
 	header("Location: /admin");
 
 	exit;
@@ -57,6 +57,70 @@ $app->get('/admin/logOut', function(){
 
 	exit;
 
+});
+
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();  //listar todos os 
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+		));
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+});
+
+$app->get("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues();
+	));
+});
+
+$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+});
+
+$app->post("/admin/users/create", function() {
+
+	User::verifyLogin();
+	
+	$user = new User();
+	
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+	
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
 });
 
 $app->run(); //roda todo o codigo depois de ter carregado tudo
